0f321ba54577d404c2452a118d798eeddcc54a33
Celine Lieutaud <celine.lieutaud@ynov.com>
2022-11-29 22:18:00 +0100

    ajout icone arrow + correction CSS

--- vitrine/pages/login.tsx
+++ vitrine/pages/login.tsx
@@ -2,7 +2,9 @@ import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import FormComponent from "../components/FormComponent";
 import HeadComponent from "../components/HeadComponent";
+import { IoIosArrowRoundBack } from "react-icons/io";
 import { HeaderComponent, FooterComponent } from 'my-lib-ui';
+
 
 const Login: NextPage = () => {
   const router = useRouter();
@@ -11,7 +13,7 @@ const Login: NextPage = () => {
       <HeadComponent />
       <HeaderComponent url="/login"/>
       <main >
-        <a href="/"><h1 className="retour_link">Retour</h1></a>
+        <a href="/"><h1 className="retour_link"><IoIosArrowRoundBack /> Retour</h1></a>
         <FormComponent formType="login"/>
       </main>
       <FooterComponent />
