f9075bf45102bdfe6dd18d291a32fc94165c2c06
Celine Lieutaud <celine.lieutaud@ynov.com>
2023-01-13 11:23:58 +0100

    finish ArrayComponent in lib, finish AdminComponent with all css, add react-icons resources in lib

--- my-lib-ui/src/components/ArrayComponent.tsx
+++ my-lib-ui/src/components/ArrayComponent.tsx
@@ -1,10 +1,51 @@
 import React from "react";
+import { IoIosCheckmark, IoMdPaw } from "react-icons/io";
 
-const ArrayComponent: React.FC = () => {
-  
+
+type ArrayProps =  { 
+  items?: User[],
+  headers?: string[]
+};
+
+type User = {
+  id: number,
+  lastname: string,
+  firstname: string,
+  email: string,
+  phone: string,
+  nationality: string,
+  isValided: boolean
+};
+
+const ArrayComponent: React.FC<ArrayProps> = (Props) => {
+  const { items, headers } = Props;
 
   return (
-    
+    <div>
+      <table className="array-component" cellPadding={10} cellSpacing={0}>
+        <thead>
+          <tr>
+            {headers != null && headers.map((header, index) => (
+              <th key={index}>{header}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {items != null && items.map(item => (
+            <tr key={item.id}>
+              <td>{item.isValided ? (<span itemID="isCheck"><IoIosCheckmark/> Validé</span>) : (<span itemID="isNotCheck"><IoMdPaw/> En attente</span>)}</td>
+              <td>{item.lastname} {item.firstname}</td>
+              <td>
+                {item.email} <br/>
+                {item.phone}
+              </td>
+              <td>{item.nationality}</td>
+              <td>Actions :smiley:</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
