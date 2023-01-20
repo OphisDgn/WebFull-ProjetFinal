f9a8a7d997104ff647469605c57fe8681086e28d
Celine Lieutaud <celine.lieutaud@ynov.com>
2023-01-20 11:05:51 +0100

    create table bdd with flask + route create + allget

--- car/app.py
+++ car/app.py
@@ -1,13 +1,42 @@
-from flask import Flask, jsonify, request
+from models import db, CarModel
+from flask import Flask, jsonify, render_template, request, redirect
 import json
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:admin@db:3306/lesson'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+@app.before_first_request
+def create_table():
+    db.create_all()
 
 @app.route("/", methods=["GET"])
-def hello():
-    return jsonify({"Hello": "World"}) 
+def getAllCars():
+    cars = CarModel.query.all()
+    return render_template("list.html", cars = cars)
 
-@app.route("/add", methods=['POST'])
-def add():
-    data = json.loads(request.data)
-    return jsonify(data["a"] + data["b"])
+
+@app.route("/create", methods=['POST'])
+def create():
+    name = request.json['name']
+    price = request.json['price']
+    image = request.json['image']
+    car = CarModel(name=name, price=price, image=image)
+    db.session.add(car)
+    db.session.commit()
+    return jsonify(car.__repr__())
+
+@app.route("/<int:id>/update", methods=['POST'])
+def update(id):
+    car = CarModel.query.filter_by(id=id).first()
+    name = request.json['name']
+    price = request.json['price']
+    image = request.json['image']
+    db.session.query(CarModel).filter(car.id == id).update(
+    {'name':name, 'price':price, 'image':image}, synchronize_session="fetch")
+    db.session.commit()
+    return jsonify(car.__repr__())
+
+
+app.run(host='localhost', port=5000)
--- car/models.py
+++ car/models.py
@@ -1,21 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
- 
+
 db = SQLAlchemy()
- 
-class EmployeeModel(db.Model):
-    __tablename__ = "table"
- 
+
+class CarModel(db.Model):
+    __tablename__ = "cars"
+
     id = db.Column(db.Integer, primary_key=True)
-    employee_id = db.Column(db.Integer(),unique = True)
-    name = db.Column(db.String())
-    age = db.Column(db.Integer())
-    position = db.Column(db.String(80))
- 
-    def __init__(self, employee_id,name,age,position):
-        self.employee_id = employee_id
-        self.name = name
-        self.age = age
-        self.position = position
- 
+    name = db.Column(db.String(50), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    image = db.Column(db.String(500), nullable=True)
+
     def __repr__(self):
-        return f"{self.name}:{self.employee_id}"
+        return f"{self.id}:{self.name}"
+
+    def __init__(self, name, price, image):
+        self.name = name
+        self.price = price
+        self.image = image
