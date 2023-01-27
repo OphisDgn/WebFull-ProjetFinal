42c76ddcf54beb57ecdee44c543c78f0754d0434
Corentin MACE <43670071+corentinmace@users.noreply.github.com>
2023-01-27 10:09:01 +0100

    parallax on login page

--- client/main.js
+++ client/main.js
@@ -1,19 +1,37 @@
-const { app, BrowserWindow } = require('electron')
+const {app, BrowserWindow} = require('electron')
 
-const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 1270,
-        height: 720,
-        resizable: false,
+app.on('ready', () => {
+  // create main browser window
+    mainWindow = new BrowserWindow({
         autoHideMenuBar: true,
+        icon: 'assets/img/logo.png',  
+        width: 1280,
+        height: 720,
+        show: false, // don't show the main window
+        resizable: false,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    // create a new `splash`-Window
+    splash = new BrowserWindow({width: 300, height: 450, icon:"assets/img/logo.png", transparent: false, frame: false});
+    splash.loadFile(`loader.html`);
+    setTimeout(function () {
+        // console.log("Loading main window")
+        mainWindow.loadURL("http://localhost:3000");
+    }, 1500);
+    
+    
+    // if main window is ready to show, then destroy the splash window and show up the main window
+    mainWindow.once('ready-to-show', () => {
+        console.log("ready")
+        splash.close();
+        mainWindow.show();
+    });
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
     })
-    win.loadURL('http://localhost:3000')
-}
-
-app.whenReady().then(() => {
-    createWindow()
-})
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-})
+    
+});
